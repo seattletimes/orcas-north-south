98df9fd62913b30a1a6148f3d5f3cab9cc19eac9
sfmina <fmina@seattletimes.com>
2018-11-09 13:24:06 -0800

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+// require("./lib/ads");
+// var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+
+//require("./calculator");
+
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+var Camera = require("savage-camera");
+var savage = require("savage-query");
+
+var keyStage = document.querySelector(".scroll-content");
+var map = document.querySelector(".backdrop svg");
+var camera = new Camera(map);
+var stages = $(".layer").reverse();
+var current = null;
+var existing = document.querySelector("#Existing");
+
+var onScroll = function() {
+  var scrollBounds = keyStage.getBoundingClientRect();
+  for (var i = 0; i < stages.length; i++) {
+    var stage = stages[i];
+    var bounds = stage.getBoundingClientRect();
+    if (bounds.top < window.innerHeight && bounds.bottom > 0) {
+      var layerID = stage.getAttribute("data-layer");
+      if (layerID == current) return;
+      var layer = document.querySelector("#" + layerID);
+      if (!layer) return;
+      if (layerID != "Existing") {
+        savage(map).addClass("zoomed");
+      } else {
+        savage(map).removeClass("zoomed");
+      }
+      var active = document.querySelector(".activated");
+      if (active) savage(active).removeClass("activated");
+      savage(layer).addClass("activated");
+      current = layerID;
+      camera.zoomTo(layer);
+      return;
+    }
+  }
+}
+
+window.addEventListener("scroll", debounce(onScroll, 500));
+onScroll();
